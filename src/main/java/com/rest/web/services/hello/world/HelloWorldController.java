0618f3f2d8fa6b9446f0a0d725c21ac4a2233654
dharmeshsojitra <sojitra_d@yahoo.co.in>
2022-08-30 21:15:37 +0530

    added hello world

---
+++ src/main/java/com/rest/web/services/hello/world/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.rest.web.services.hello.world;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World!!";
+    }
+
+}
