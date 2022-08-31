1dfd1a9571eefac430edf6691264aebe3215e480
dharmeshsojitra <sojitra_d@yahoo.co.in>
2022-08-31 20:02:07 +0530

    returning object from rest controller

--- src/main/java/com/rest/web/services/hello/world/HelloWorldController.java
+++ src/main/java/com/rest/web/services/hello/world/HelloWorldController.java
@@ -12,4 +12,8 @@ public class HelloWorldController {
         return "Hello World!!";
     }
 
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World!!");
+    }
 }
