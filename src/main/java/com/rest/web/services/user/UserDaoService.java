38245da9f02aa4fe83eee52f42f3b3606da61659
dharmeshsojitra <sojitra_d@yahoo.co.in>
2022-09-08 18:11:33 +0530

    added user module

---
+++ src/main/java/com/rest/web/services/user/UserDaoService.java
@@ -0,0 +1,48 @@
+package com.rest.web.services.user;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+
+    private static List<User> users = new ArrayList<>();
+
+    private static int usersCount = 0;
+
+    static {
+        users.add(
+                new User(++usersCount, "Adam", LocalDate.now().minusYears(30)));
+        users.add(
+                new User(++usersCount, "Eva", LocalDate.now().minusYears(35)));
+        users.add(
+                new User(++usersCount, "John", LocalDate.now().minusYears(40)));
+        users.add(
+                new User(++usersCount, "Jim", LocalDate.now().minusYears(20)));
+        users.add(
+                new User(++usersCount, "Luna", LocalDate.now().minusYears(25)));
+        users.add(
+                new User(++usersCount, "Mark", LocalDate.now().minusYears(26)));
+
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User findById(int id) {
+        return users.stream().filter(e -> e.getId().equals(id))
+                .findFirst().get();
+    }
+
+    public User saveUser(User user) {
+
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
+    }
+
+}
